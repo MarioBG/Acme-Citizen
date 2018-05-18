e3e05a6616bc07152381d7632755ecc66be5898e
Juan Rodríguez Dueñas <juaroddue@alum.us.es>
2018-05-18 17:59:43 +0200

    Added Juan's domain classes

--- L12 Workspace/Acme-Citizen/src/main/java/domain/Chirp.java
+++ L12 Workspace/Acme-Citizen/src/main/java/domain/Chirp.java
@@ -16,15 +16,14 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.SafeHtml;
-import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
 @Table(indexes = {
-		@Index(columnList = "user_id")
-	})
+	@Index(columnList = "govermentAgent_id")
+})
 public class Chirp extends DomainEntity {
 
 	// Constructors
@@ -36,24 +35,14 @@ public class Chirp extends DomainEntity {
 
 	// Attributes
 
-	private Date	publicationMoment;
 	private String	title;
-	private String	description;
+	private String	content;
+	private Date	publicationDate;
+	private String	image;
+	private String	link;
 
-
-	@Past
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "dd/MM/yyyy")
-	public Date getPublicationMoment() {
-		return this.publicationMoment;
-	}
-
-	public void setPublicationMoment(final Date publicationMoment) {
-		this.publicationMoment = publicationMoment;
-	}
 
 	@NotBlank
-	@SafeHtml(whitelistType = WhiteListType.NONE)
 	public String getTitle() {
 		return this.title;
 	}
@@ -63,30 +52,59 @@ public class Chirp extends DomainEntity {
 	}
 
 	@NotBlank
-	@SafeHtml(whitelistType = WhiteListType.NONE)
-	public String getDescription() {
-		return this.description;
+	public String getContent() {
+		return this.content;
 	}
 
-	public void setDescription(final String description) {
-		this.description = description;
+	public void setContent(final String content) {
+		this.content = content;
+	}
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	public Date getPublicationDate() {
+		return this.publicationDate;
+	}
+
+	public void setPublicationDate(final Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	@URL
+	public String getImage() {
+		return this.image;
+	}
+
+	public void setImage(final String image) {
+		this.image = image;
+	}
+
+	@URL
+	public String getLink() {
+		return this.link;
+	}
+
+	public void setLink(final String link) {
+		this.link = link;
 	}
 
 
 	// Relationships
 
-	private User	user;
+	private GovernmentAgent	governmentAgent;
 
 
 	@Valid
 	@NotNull
 	@ManyToOne(optional = false)
-	public User getUser() {
-		return this.user;
+	public GovernmentAgent getGovernmentAgent() {
+		return this.governmentAgent;
 	}
 
-	public void setUser(final User user) {
-		this.user = user;
+	public void setGovernmentAgent(final GovernmentAgent governmentAgent) {
+		this.governmentAgent = governmentAgent;
 	}
 
 }
