6b20f068375260e52f367bfda59ee742db04cb78
Juan Rodríguez Dueñas <juaroddue@alum.us.es>
2018-06-04 20:20:14 +0200

    Closed #3

--- L12 Workspace/Acme-Citizen/src/main/java/controllers/PetitionController.java
+++ L12 Workspace/Acme-Citizen/src/main/java/controllers/PetitionController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,6 +50,7 @@ public class PetitionController extends AbstractController {
 		Citizen citizen = null;
 
 		if (citizenId != null) {
+			Assert.notNull(this.citizenService.findOne(citizenId));
 			citizen = this.citizenService.findOne(citizenId);
 			petitions = this.petitionService.findByCitizenId(citizenId);
 		} else
